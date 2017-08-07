8ad1660f4c2cbff5dc3207623dc13a42e15d011d
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-07 15:42:42 +0300

    lecture yargs

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 console.log('starting app.js')
 
+require('dotenv').config()
 const fs = require('fs')
-const os = require('os')
 const _ = require('lodash')
+
 const notes = require('./notes')
 
-// console.log(_.isString(true))
-// console.log(_.isString('BOb5'))
-let filteredArray = _.uniq(['Bob', 1, 'Bob', 1, 2, 3, 4])
-console.log(filteredArray)
+let command = process.argv[2]
+console.log('Command:', command)
+console.log(process.argv)
 
-// console.log(notes.add('hello ', 'world'))
-// console.log(notes.add(5, -3))
 
-// let user = os.userInfo()
-
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`)
+if (command === 'add') {
+  console.log('Adding new note')
+} else if (command === 'list') {
+  console.log('Listing all notes')
+} else if (command === 'read') {
+  console.log('Fetching note')
+} else if (command === 'remove') {
+  console.log('Removing note')
+} else {
+  console.log('Command not recognized')
+}
