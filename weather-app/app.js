4548f373994699b35018a099ddb2440c9b9020d6
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-09 00:54:37 +0300

    start L.32

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,27 @@
-const request = require('request')
+const request = require('request'),
+      yargs   = require('yargs')
 
-const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia'
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to get weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv
+
+const apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+const url = apiUrl + encodeURIComponent(argv.a)
 
 request({
   url,
   json: true
 }, (error, response, body) => {
-  console.log('latitude: ', body.results[0].formatted_address)
+  console.log('formatted_address: ', body.results[0].formatted_address)
   console.log('latitude: ', body.results[0].geometry.location.lat)
   console.log('longitude: ', body.results[0].geometry.location.lng)
 })
