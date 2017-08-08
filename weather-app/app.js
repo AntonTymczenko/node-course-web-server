7973912fdf2067ec20138abd9c98bec88fda8635
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-09 01:57:53 +0300

    start L.35

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,9 @@
 const yargs = require('yargs'),
   geocode = require('./geocode/geocode')
 
+const request = require('request')
+const weatherApiUrl = 'https://api.darksky.net/forecast/184dd35e027ab591a8ba26fd27bdbc5e/'
+
 const argv = yargs
   .options({
     address: {
@@ -18,6 +21,18 @@ const argv = yargs
     if (err) {
       console.log(err)
     } else {
-      console.log(JSON.stringify(res, undefined, 2))
+      // console.log(JSON.stringify(res, undefined, 2))
+      const weatherUrl = weatherApiUrl + res.latitude + ',' + res.longitude
+      request({
+        url: weatherUrl,
+        json: true
+      }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          console.log(JSON.stringify(body.currently.temperature, undefined, 2))
+        } else {
+          console.log('Unable to get weather')
+          console.log(error)
+        }
+      })
     }
   })
