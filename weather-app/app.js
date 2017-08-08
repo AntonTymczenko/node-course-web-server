cedfef2e1859f06aa31f039797a7cbe2fa2d23e0
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-08 23:06:43 +0300

    start L.31

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,6 +6,7 @@ request({
   url,
   json: true
 }, (error, response, body) => {
-  console.log('latitude: ', JSON.stringify(body.results[0].geometry.location.lat, undefined, 2))
-  console.log('longitude: ', JSON.stringify(body.results[0].geometry.location.lng, undefined, 2))
+  console.log('latitude: ', body.results[0].formatted_address)
+  console.log('latitude: ', body.results[0].geometry.location.lat)
+  console.log('longitude: ', body.results[0].geometry.location.lng)
 })
