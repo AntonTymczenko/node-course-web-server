d2764706d02835a17abac6d0656f2c774169f9e9
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-10 02:31:51 +0300

    start lesson 45

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,20 +2,29 @@ const express = require('express'),
       hbs     = require('hbs')
 
 const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+// app.set('views', __dirname + '/views')
 app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
 
 app.get('/', (req, res) => {
   res.render('home', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Hello my dear friend and welcome to my web site! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Hello my dear friend and welcome to my web site! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   })
 })
 app.get('/about', (req, res) => {
   res.render('about', {
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   })
 })
 
