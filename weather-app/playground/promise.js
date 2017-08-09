3e974fe832a68f1d5694c9fd73b7a4d9ffedcdf4
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-09 16:08:49 +0300

    start L.38

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,17 +1,41 @@
-let somePromise = new Promise ((resolve, reject) => {
-  setTimeout(() => {
-    resolve('hey, it worked')
-    // reject('unable to fulfill promise')
-  }, 2500)
-})
+let asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve (a + b)
+      } else {
+        reject('Arguments must be numbers')
+      }
+    }, 1500)
+  })
+}
 
-let anotherPromise = new Promise ((resolve, reject) => {
-
-})
-
-somePromise.then((msg) => {
-    console.log('Success: ', msg)
+asyncAdd(4,3.4)
+  .then((res) => {
+    console.log('result: ', res)
+    return asyncAdd(res, 33)
+  })
+  .then ((res) => {
+    console.log('another result: ', res)
   })
   .catch((err) => {
-    console.log('Failed: ', err)
+    console.log(err)
   })
+
+// let somePromise = new Promise ((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('hey, it worked')
+//     // reject('unable to fulfill promise')
+//   }, 2500)
+// })
+//
+// let anotherPromise = new Promise ((resolve, reject) => {
+//
+// })
+//
+// somePromise.then((msg) => {
+//     console.log('Success: ', msg)
+//   })
+//   .catch((err) => {
+//     console.log('Failed: ', err)
+//   })
