73554616826049215d643e22d001ea04339f907b
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-10 01:43:19 +0300

    start ADVANCED TEMPLATING

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,22 @@
-const express = require('express')
+const express = require('express'),
+      hbs     = require('hbs')
 
 const app = express()
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello from express.js</h1>')
-  // /*
-  res.send({
-    name: 'Robert Paulson',
-    likes: [
-      'hiking',
-      'cooking'
-    ]
+  res.render('home', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hello my dear friend and welcome to my web site! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    currentYear: new Date().getFullYear()
   })
-  // */
 })
 app.get('/about', (req, res) => {
-  res.send('<h1>About page</h1>')
+  res.render('about', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
 })
 
 app.get('/bad', (req, res) => {
