334533fdb6be80c36f6ce019a595f169e99c2044
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-11 00:47:57 +0300

    start lesson 54

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,5 +1,33 @@
-const utils = require('./utils')
+const utils = require('./utils'),
+    expect  = require('expect')
 
 it('should add two numbers', () => {
-  let res = utils.add(33, 11)  
+  expect(utils.add(33, 11)).toBe(44).toBeA('number')
+})
+
+it('should square a number', () => {
+  expect(utils.square(3)).toBe(9).toBeA('number')
+})
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(11)
+  // expect({name: 'Andrew'}).toNotEqual({name: 'Andrew'})
+  // expect([2,3,4]).toExclude(1)
+  expect({
+    name: 'Andrew',
+    age: 25,
+    location: 'Philadelphia'
+  }).toExclude({
+    age: 23
+  })
+})
+
+//should verify first and last names are set
+
+it('should have first and last names set', () => {
+  let user = {}
+  expect(utils.setName(user, 'Anton Tymczenko'))
+    .toBeA('object')
+    .toInclude({firstName: 'Anton'})
+    .toInclude({lastName: 'Tymczenko'})
 })
