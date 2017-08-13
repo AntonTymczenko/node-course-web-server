68fd75df7f4fe68235053755f53a83a17cf331b8
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-13 20:43:33 +0300

    start lesson 68

--- todo-api/playground/mongodb-connect.js
+++ todo-api/playground/mongodb-connect.js
@@ -7,17 +7,17 @@ MongoClient.connect('mongodb://localhost:27017/todo', (err, db) => {
   }
   console.log('connected to MongoDB server', err)
 
-  // db.collection('Todos').insertOne({
-  //   text: 'Something else to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if (err){
-  //     return console.log('Error: Unable to insert TODO', err)
-  //   }
-  //
-  //   console.log(JSON.stringify(result.ops, undefined, 2))
-  //
-  // })
+  db.collection('Todos').insertOne({
+    text: 'Eat lunch',
+    completed: false
+  }, (err, result) => {
+    if (err){
+      return console.log('Error: Unable to insert TODO', err)
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2))
+
+  })
 
 // Insert new doc into Users (name, age, location)
 
