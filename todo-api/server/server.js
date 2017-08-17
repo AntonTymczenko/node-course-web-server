c784ced050a1b64b890d61467666298215341269
Anton Tymczenko <tymczenko@protonmail.com>
2017-08-17 15:23:49 +0300

    start lesson 79

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -48,7 +48,7 @@ app.get('/todos/:id', (req, res) => {
       if (!todo) {
         return res.status(404).send('Not found')
       }
-      res.send(todo)
+      res.status(200).send({todo})
     })
     .catch((err) => {
       res.status(500).send('Error')
